8cd70cc23caf4d1d306f188d886574c755795a33
mphojames9 <98138271+mphojames9@users.noreply.github.com>
2022-04-17 20:00:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,102 +1,132 @@
 
-const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
-    let moves = 0;
- 
-    const playGame = () => {
-        const rockBtn = document.querySelector('.rock');
-        const paperBtn = document.querySelector('.paper');
-        const scissorBtn = document.querySelector('.scissor');
-        const playerOptions = [rockBtn,paperBtn,scissorBtn];
-        const computerOptions = ['rock','paper','scissors']
-         
-        playerOptions.forEach(option => {
-            option.addEventListener('click',function(){
- 
-                const movesLeft = document.querySelector('.movesleft');
-                moves++;
-                movesLeft.innerText = `Moves Left: ${10-moves}`;
-                 
- 
-                const choiceNumber = Math.floor(Math.random()*3);
-                const computerChoice = computerOptions[choiceNumber];
- 
-                winner(this.innerText,computerChoice)
-                 
-                if(moves == 10){
-                    gameOver(playerOptions,movesLeft);
-                }
-            })
-        })
-         
-    }
+let userScore = 0;
+let comuputerScore = 0;
+let moves = 0;
+const userScore_span = document.getElementById('p-count count');
+const computerScore_span = document.getElementById('c-count count');
+const result = document.getElementById('result');
+const rock = document.getElementById('r');
+const paper = document.getElementById('p');
+const sissors = document.getElementById('s');
 
-    const winner = (player,computer) => {
-        const result = document.querySelector('.result');
-        const playerScoreBoard = document.querySelector('.p-count');
-        const computerScoreBoard = document.querySelector('.c-count');
-        player = player.toLowerCase();
-        computer = computer.toLowerCase();
-        if(player === computer){
-            result.textContent = 'Tie'
-        }
-        else if(player == 'rock'){
-            if(computer == 'paper'){
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
- 
-            }else{
-                result.textContent = 'Player Won'
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-        }
-        else if(player == 'scissors'){
-            if(computer == 'rock'){
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            }else{
-                result.textContent = 'Player Won';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-        }
-        else if(player == 'paper'){
-            if(computer == 'scissors'){
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            }else{
-                result.textContent = 'Player Won';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-        }
+//Computer choice function
+function getComputerChoice(){
+    choices = ['r','p','s'];
+   randomNumbers = [Math.floor(Math.random()*3)];
+   return choices[randomNumbers]
+}
+
+function win(){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    result.innerHTML = 'You win!'
+    result.style.fontSize = '1.5rem';
+    result.style.color = 'White';
+    document.getElementById('mini-screen').classList.add('green-glow');  
+    setTimeout(function() { document.getElementById("mini-screen").classList.remove('green-glow')}, 1000);
+}
+
+
+function loss(){
+    comuputerScore++;
+    computerScore_span.innerHTML = comuputerScore;
+    result.innerHTML = 'Computer wins'
+    result.style.fontSize = '1.5rem';
+    result.style.color = 'White';
+    document.getElementById('mini-screen').classList.add('red-glow');  
+    setTimeout(function() { document.getElementById("mini-screen").classList.remove('red-glow')}, 1000);
+}
+
+function draw(){
+    result.innerHTML = 'Its a draw!'
+    result.style.fontSize = '1.5rem';
+    result.style.color = 'White';
+    document.getElementById('mini-screen').classList.add('blue-glow');  
+    setTimeout(function() { document.getElementById("mini-screen").classList.remove('blue-glow')}, 1000);
+}
+
+//User choice function
+function game(userChoice) {
+    const computerChoice = getComputerChoice()
+    switch (userChoice + computerChoice) {
+        case'rp':
+        case'pr':
+        case'sr':
+        win()
+        break;
+
+        case'rs':
+        case'ps':
+        case'sp':
+        loss();
+        break;
+
+        case'rr':
+        case'pp':
+        case'ss':
+        draw();
+        break;
     }
- 
-    const gameOver = (playerOptions,movesLeft) => {
+};
+
+//Main function
+rock.addEventListener('click', function(){
+    game('r');
+
+    const movesLeft = document.querySelector('.movesleft');
+    moves++;
+    movesLeft.innerText = `Moves Left: ${10-moves}`;
+
+    if(moves == 10){
+        gameOver(game,movesLeft);
+
+
+}
+});
+
+paper.addEventListener('click', function(){
+    game('p');
+    const movesLeft = document.querySelector('.movesleft');
+    moves++;
+    movesLeft.innerText = `Moves Left: ${10-moves}`;
+
+    if(moves == 10){
+        gameOver(game,movesLeft);
+}
+
+    
+});
+sissors.addEventListener('click', function(){
+    game('s');
+    const movesLeft = document.querySelector('.movesleft');
+    moves++;
+    movesLeft.innerText = `Moves Left: ${10-moves}`;
+
+    if(moves == 10){
+        gameOver(game,movesLeft);
+}
+
+});
+
+  const gameOver = (playerOptions,movesLeft) => {
  
         const chooseMove = document.querySelector('.move');
         const result = document.querySelector('.result');
         const reloadBtn = document.querySelector('.reload');
  
-        playerOptions.forEach(option => {
+        game(option => {
             option.style.display = 'none';
         })
- 
       
         chooseMove.innerText = 'Game Over!!'
         movesLeft.style.display = 'none';
  
-        if(playerScore > computerScore){
+        if(userScore_span > computerScore_span){
             result.style.fontSize = '1rem';
             result.innerText = 'You Won The Game'
             result.style.color = '#308D46';
         }
-        else if(playerScore < computerScore){
+        else if(userScore_span < computerScore_span){
             result.style.fontSize = '1rem';
             result.innerText = 'You Lost The Game';
             result.style.color = 'red';
@@ -112,7 +142,3 @@ const game = () => {
             window.location.reload();
         })
     }
- 
-    playGame();
-}
-game();
