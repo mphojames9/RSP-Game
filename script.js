1f2adc26a2f4286afd2ff519943c8463db7b4178
mphojames9 <98138271+mphojames9@users.noreply.github.com>
2022-07-30 08:29:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,74 @@
-
 let userScore = 0;
-let comuputerScore = 0;
+let computerScore = 0;
 let moves = 0;
 const userScore_span = document.getElementById('p-count count');
 const computerScore_span = document.getElementById('c-count count');
+const miniScreen = document.getElementById('mini-screen');
 const result = document.getElementById('result');
 const rock = document.getElementById('r');
 const paper = document.getElementById('p');
 const sissors = document.getElementById('s');
+const reloadBtn = document.querySelector('.reload');
+const gameOverText = document.querySelector('.move');
+const movesLeft = document.getElementById('movesleft');
+const optionsBtn = document.querySelector('.options');
 
-//Computer choice function
-function getComputerChoice(){
-    choices = ['r','p','s'];
-   randomNumbers = [Math.floor(Math.random()*3)];
-   return choices[randomNumbers]
-}
-
+//results functions
 function win(){
     userScore++;
-    userScore_span.innerHTML = userScore;
-    result.innerHTML = 'You win!'
-    result.style.fontSize = '1.5rem';
-    result.style.color = 'White';
-    document.getElementById('mini-screen').classList.add('green-glow');  
-    setTimeout(function() { document.getElementById("mini-screen").classList.remove('green-glow')}, 1000);
+    userScore_span.innerText = userScore;
+    result.innerText = 'You win';
+    miniScreen.classList.add('green-glow');
+    setTimeout(function greenGlow (){
+        miniScreen.classList.remove('green-glow')
+    }, 1000);
+    
+
 }
 
-
 function loss(){
-    comuputerScore++;
-    computerScore_span.innerHTML = comuputerScore;
-    result.innerHTML = 'Computer wins'
-    result.style.fontSize = '1.5rem';
-    result.style.color = 'White';
-    document.getElementById('mini-screen').classList.add('red-glow');  
-    setTimeout(function() { document.getElementById("mini-screen").classList.remove('red-glow')}, 1000);
+    computerScore++;
+    computerScore_span.innerText = computerScore;
+    result.innerText = 'Computer wins';
+    miniScreen.classList.add('red-glow');
+    setTimeout(function redGlow (){
+        miniScreen.classList.remove('red-glow')},1000
+    );
+
+
+
 }
 
 function draw(){
-    result.innerHTML = 'Its a draw!'
-    result.style.fontSize = '1.5rem';
-    result.style.color = 'White';
-    document.getElementById('mini-screen').classList.add('blue-glow');  
-    setTimeout(function() { document.getElementById("mini-screen").classList.remove('blue-glow')}, 1000);
+    result.innerText = 'Draw';
+    miniScreen.classList.add('blue-glow');
+    setTimeout(function redGlow (){
+        miniScreen.classList.remove('blue-glow')},1000
+    );
+
 }
 
-//User choice function
+//computer choice function
+function getComputerChoice(){
+   let choices = ['r','p','s'];
+   let randomNumbers = Math.floor(Math.random()*3);
+    return choices[randomNumbers];
+}
+
+
+//user choice function
 function game(userChoice) {
-    const computerChoice = getComputerChoice()
+    const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
-        case'rp':
+        case'rs':
         case'pr':
-        case'sr':
-        win()
+        case'sp':
+        win();
         break;
 
-        case'rs':
+        case'rp':
         case'ps':
-        case'sp':
+        case'sr':
         loss();
         break;
 
@@ -66,79 +77,73 @@ function game(userChoice) {
         case'ss':
         draw();
         break;
+
     }
-};
-
-//Main function
-rock.addEventListener('click', function(){
-    game('r');
-
-    const movesLeft = document.querySelector('.movesleft');
-    moves++;
-    movesLeft.innerText = `Moves Left: ${10-moves}`;
-
-    if(moves == 10){
-        gameOver(game,movesLeft);
-
-
-}
-});
-
-paper.addEventListener('click', function(){
-    game('p');
-    const movesLeft = document.querySelector('.movesleft');
-    moves++;
-    movesLeft.innerText = `Moves Left: ${10-moves}`;
-
-    if(moves == 10){
-        gameOver(game,movesLeft);
-}
-
     
-});
-sissors.addEventListener('click', function(){
-    game('s');
-    const movesLeft = document.querySelector('.movesleft');
-    moves++;
-    movesLeft.innerText = `Moves Left: ${10-moves}`;
-
-    if(moves == 10){
-        gameOver(game,movesLeft);
 }
 
+//main function
+rock.addEventListener('click', function (){
+    game('r');
+    let movesLeft = document.getElementById('movesleft');
+    moves++;
+    movesLeft.innerText = `Moves Left: ${10-moves}`;
+    if(moves == 10){
+        gameOver();
+    }
+
 });
 
-  const gameOver = (playerOptions,movesLeft) => {
- 
-        const chooseMove = document.querySelector('.move');
-        const result = document.querySelector('.result');
-        const reloadBtn = document.querySelector('.reload');
- 
-        game(option => {
-            option.style.display = 'none';
-        })
-      
-        chooseMove.innerText = 'Game Over!!'
-        movesLeft.style.display = 'none';
- 
-        if(userScore_span > computerScore_span){
-            result.style.fontSize = '1rem';
-            result.innerText = 'You Won The Game'
-            result.style.color = '#308D46';
-        }
-        else if(userScore_span < computerScore_span){
-            result.style.fontSize = '1rem';
-            result.innerText = 'You Lost The Game';
-            result.style.color = 'red';
-        }
-        else{
-            result.style.fontSize = '2rem';
-            result.innerText = 'Tie';
-            result.style.color = 'white'
-        }
-        reloadBtn.innerText = 'Restart';
-        reloadBtn.style.display = 'grid'
-        reloadBtn.addEventListener('click',() => {
-            window.location.reload();
-        })
+paper.addEventListener('click', function (){
+    game('p');
+    let movesLeft = document.getElementById('movesleft');
+    moves++;
+    movesLeft.innerText = `Moves Left: ${10-moves}`;
+    if(moves == 10){
+        gameOver();
     }
+});
+
+sissors.addEventListener('click', function (){
+    game('s');
+    let movesLeft = document.getElementById('movesleft');
+    moves++;
+    movesLeft.innerText = `Moves Left: ${10-moves}`;
+    if(moves == 10){
+        gameOver();
+    }
+});
+
+function gameOver() {
+
+    if(userScore > computerScore){
+        result.style.fontSize = '1rem';
+        result.innerText = 'You Won The Game'
+        result.style.color = '#308D46';
+    }
+
+    else if(userScore < computerScore){
+        result.style.fontSize = '1rem';
+        result.innerText = 'You Lost The Game';
+        result.style.color = 'red';
+    }
+
+    else{
+        result.style.fontSize = '2rem';
+        result.innerText = 'Tie';
+        result.style.color = 'white'
+    }
+
+    optionsBtn.style.display = 'none';
+
+
+
+    reloadBtn.innerText = 'Restart';
+    reloadBtn.style.display = 'grid'
+    reloadBtn.addEventListener('click',function() {
+        window.location.reload();
+    });
+
+    movesLeft.style.display = 'none';
+    gameOverText.innerText = 'Game Over!';
+}
