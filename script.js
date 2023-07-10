339fc19b93d78eec11042ef2d0e8d9fa35750a62
Mpho Matli <mphojames9@gmail.com>
2023-07-10 12:36:15 +0200

    update

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let playerScore = 0;
 let computerScore = 0;
 let moves = 1;
 const playerScore_display = document.querySelector(".playerScoreCounter");
-const results = document.querySelector("#tempResults");
+const results = document.querySelector(".temporaryResults");
 const rounds = document.querySelector(".roundCounter-span");
+const computerChoice_backGround = document.querySelector(".computerChoice");
 const computerScore_display = document.querySelector(".computerScoreCounter");
 const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
@@ -11,14 +12,22 @@ const sissorBtn = document.querySelector("#sissor");
 
 rockBtn.addEventListener("click",()=>{
     main('rock');
+    paperBtn.style="background-image: none";
+    sissorBtn.style="background-image: none";
+
+
 })
 
 paperBtn.addEventListener("click",()=>{
     main('paper');
+    sissorBtn.style="background-image: none";
+    rockBtn.style="background-image: none";
 })
 
 sissorBtn.addEventListener("click",()=>{
     main('sissors');
+    paperBtn.style="background-image: none";
+    rockBtn.style="background-image: none";
 })
 
 function computerSelection(){
@@ -30,22 +39,31 @@ function computerSelection(){
 function main(playerSelection){
     const computerChoice = computerSelection();
     if(playerSelection === "rock" && computerChoice === "rock"){
+            computerChoice_backGround.style = "background-image: url(img/rock.png)";
         tie();
     }else if(playerSelection === "rock" && computerChoice === "paper"){
+        computerChoice_backGround.style = "background-image: url(img/paper.png)";
         loss();
     }else if(playerSelection === "rock" && computerChoice === "sissors"){
+        computerChoice_backGround.style = "background-image: url(img/sissor.png)";
         win();
     }else if(playerSelection === "paper" && computerChoice === "rock"){
+        computerChoice_backGround.style = "background-image: url(img/rock.png)";
         win();
     }else if(playerSelection === "paper" && computerChoice === "paper"){
+        computerChoice_backGround.style = "background-image: url(img/paper.png)";
         tie();
     }else if(playerSelection === "paper" && computerChoice === "sissors"){
+        computerChoice_backGround.style = "background-image: url(img/sissor.png)";
         loss();
     }else if(playerSelection === "sissors" && computerChoice === "rock"){
+        computerChoice_backGround.style = "background-image: url(img/rock.png)";
         loss();
     }else if(playerSelection === "sissors" && computerChoice === "paper"){
+        computerChoice_backGround.style = "background-image: url(img/paper.png)";
         win();
     }else if(playerSelection === "sissors" && computerChoice === "sissors"){
+        computerChoice_backGround.style = "background-image: url(img/sissor.png)";
         tie();
     }
     if(rounds.innerHTML === "5"){
@@ -53,21 +71,40 @@ function main(playerSelection){
     }
     moves++
 }
-
 function win(){
     playerScore++;
     playerScore_display.innerHTML = playerScore;
     results.innerHTML = "You win";
     rounds.innerHTML = `${moves}`;
+    displayElements();
+
 }
 function loss(){
     computerScore++;
     computerScore_display.innerHTML = computerScore;
     results.innerHTML = "You lose";
     rounds.innerHTML = `${moves}`;
+    displayElements();
 }
 
 function tie(){
     results.innerHTML = "Tie";
     rounds.innerHTML = `${moves}`;
+    displayElements();
 }
+
+function displayElements(){
+    results.style.display  = 'block';
+    function test(){
+        results.style.display = "none"
+    }
+    function restoreBackImg(){
+    rockBtn.style="background-image: (img/rock.png)";
+    paperBtn.style="background-image: (img/paper.png)";
+    sissorBtn.style="background-image: (img/sissor.png)";
+    computerChoice_backGround.style = "background-image: none";
+    }
+    setTimeout(test,1500);
+    setTimeout(restoreBackImg,1500);
+}
+
